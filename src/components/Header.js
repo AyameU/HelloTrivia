b63a0c384a75b1112bea0e8d3599c0b44328eda7
AyameU <54376512+AyameU@users.noreply.github.com>
2021-10-22 19:49:45 -0500

    Add icon to the header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import { BsFillPatchQuestionFill } from "react-icons/bs";
+
 export default function Header() {
   return (
     <header className="content">
-      <h1 className="title has-text-centered is-1">Hello, Trivia</h1>
+      <h1 className="title has-text-centered is-1">
+        <BsFillPatchQuestionFill />
+        &nbsp; Hello, Trivia
+      </h1>
     </header>
   );
 }

87e1d4e4e3e3c88a35893767612487e43f69d5fd
AyameU <54376512+AyameU@users.noreply.github.com>
2021-10-27 01:17:12 -0500

    Add CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,12 @@ import { BsFillPatchQuestionFill } from "react-icons/bs";
 export default function Header() {
   return (
     <>
-      <header className="content has-text-centered">
-        <div>
-          <h1 className="title is-1 is-flex is-align-items-center is-justify-content-center">
-            <BsFillPatchQuestionFill />
-            &nbsp; Hello, Trivia
-          </h1>
-          <p className="subtitle">Do You Want to Play a Game?</p>
+      <header className="has-text-centered">
+        <div className="is-flex is-align-items-center is-justify-content-center">
+        <BsFillPatchQuestionFill className="mr-2" size="3rem" color="#363636" />
+          <h1 className="title is-1">Hello, Trivia</h1>
         </div>
+        <p className="subtitle">Do You Want to Play a Game?</p>
       </header>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import "./styles.css";
 import "bulma";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
